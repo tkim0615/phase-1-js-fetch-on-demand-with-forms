6493e6143b386d68b31a6f31036f2ac9897cf614
tyler kim <yoonsoo.kim11@gmail.com>
2023-11-22 17:59:35 -0500

    add solution including if else statement and alert message if movie id not found

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
 const init = () => {
-  
+
+    const inputForm = document.querySelector("form");
+
+  inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const input = document.querySelector("input#searchByID")   ;
+
+    fetch(`http://127.0.0.1:3000/movies/${input.value}`)
+    .then(response => {
+        if(response.ok){
+            return response.json()
+        }
+        else{
+           return alert('Movie not found')
+        }
+    })
+    .then(data => {
+        
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+        title.textContent = data.title
+        summary.textContent = data.summary
+
+
+
+    })
+
+
+
+  });
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
